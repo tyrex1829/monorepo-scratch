e76d4cdc488b03c16511577e4e57165be69addb2
Saksham Khare <sakshamkhare19@gmail.com>
2025-07-18 10:12:28 +0530

    Feat: main: custom comp in packages for web

---
+++ turborepo-with-pnpm/apps/web/app/admin/page.tsx
@@ -0,0 +1,6 @@
+import { Admin } from "@repo/ui/admin";
+import React from "react";
+
+export default function page() {
+  return <Admin />;
+}
---
+++ turborepo-with-pnpm/packages/ui/src/admin.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Admin() {
+  return (
+    <div>
+      <h1>This is admin page</h1>
+    </div>
+  );
+}
